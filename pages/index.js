3fd2072021f2946ffdd12ec73fbc331d8f3b8d6a
Qin <34093925+wwqin@users.noreply.github.com>
2019-07-08 16:44:30 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { connect } from 'react-redux';
 import store from '../store';
 import Link from 'next/link';
+import Head from 'next/head';
 // 页面
 
 class App extends React.Component{
@@ -11,6 +12,10 @@ class App extends React.Component{
 	render(){
 		return (
 			<div>
+			    <Head>
+				<title>SACC of Integrated system</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			    </Head>
 				sacc
 				<Link href="/#/management/management">
 					<a>here</a>
