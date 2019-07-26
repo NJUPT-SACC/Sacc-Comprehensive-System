a15e4f066b35a5435253247ca75e273d97c4e991
HKFPouu <389746410@qq.com>
2019-07-26 12:06:32 +0800

    fix: 基本资料

--- pages/home/PersonCenter/components/BasicInformation.js
+++ pages/home/PersonCenter/components/BasicInformation.js
@@ -1,112 +1,76 @@
 import React from 'react';
-import '../less/basicInformation.less'
+import '../less/basicInformation.less';
 class BasicInformation extends React.Component{
 
   constructor(props){
     super(props)
     this.state={
       flag: false,
-      nickName: 'ouu',
-      name:'张颖',
-      studentId:'B18030406',
-      group: '前端组',
-      mail: '389746410@qq.com',
-      college:'计算机学院、软件学院、网络空间安全学院',
-      major: '计算机科学与技术',
-      grade: '大一'
+      BasicInformationList: [
+        {
+          key: '昵称',
+          value: 'ouu'
+        },{
+          key:'姓名',
+          value: '张颖'
+        },{
+          key: '学号',
+          value:'B18030406'
+        },{
+          key: '组别',
+          value: '前端组'
+        },{
+          key: '邮箱',
+          value: '389746410@qq.com'
+        },{
+          key: '学院',
+          value: '计算机学院、软件学院、网络空间安全学院'
+        },{
+          key: '专业',
+          value: '计算机科学与技术'
+        },{
+          key: '年级',
+          value: '大一'
+        }
+      ]
     }
   }
 
-  Editor =()=>{
+  changeValue = (e) => {
+    let List = this.state.BasicInformationList;
+    List.map((obj) =>  {
+      if(e.target.name == obj.key){
+        obj.value = e.target.value;
+      }
+    })
     this.setState({
-      flag: true
+      BasicInformationList: List
     })
   }
 
-  changeName = (e)=>{
-    switch(e.target.name){
-      case 'name':
-        this.setState({
-          name:e.target.value
-        })
-        break;
-      case 'nickName':
-        this.setState({
-          nickName:e.target.value
-        })
-        break;
-      case 'studentId':
-        this.setState({
-          studentId:e.target.value
-        })
-        break;
-      case 'group':
-        this.setState({
-          group:e.target.value
-        })
-        break;      
-      case 'mail':
-        this.setState({
-          mail:e.target.value
-        })
-        break;      
-      case 'college':
-        this.setState({
-          college:e.target.value
-        })
-        break;      
-      case 'major':
-        this.setState({
-          major:e.target.value
-        }) 
-        break;     
-      case 'grade':
-        this.setState({
-          grade:e.target.value
-        })
-        break;
-    }
+  Editor = () => {
+    let currentFlag = this.state.flag;
+    this.setState({
+      flag: !currentFlag
+    })
   }
 
   render(){
     return(
       <div className="BasicInformation">
         <div className="BasicInformationCaption">
-          <button onClick={this.Editor}>编辑</button>
+          <p>基本信息</p>
+          <button onClick={this.Editor}>{this.state.flag?'保存':'编辑'}</button>
         </div>
-        <div>
-          <dl>
-            <dt>昵称</dt>
-            <dd>{this.state.flag ? <input value = {this.state.nickName} onChange={this.changeName} name="nickName"/>: this.state. nickName}</dd>
-          </dl>
-          <dl>
-            <dt>姓名</dt>
-            <dd>{this.state.flag ? <input value = {this.state.name} onChange={this.changeName} name="name"/>: this.state.name}</dd>
-          </dl>
-          <dl>
-            <dt>学号</dt>
-            <dd>{this.state.flag ? <input value = {this.state.studentId} onChange={this.changeName} name="studentId"/>: this.state.studentId}</dd>
-          </dl>
-          <dl>
-            <dt>组别</dt>
-            <dd>{this.state.flag ? <input value = {this.state.group} onChange={this.changeName} name="group"/>: this.state.group}</dd>
-          </dl>
-          <dl>
-            <dt>邮箱</dt>
-            <dd>{this.state.flag ? <input value = {this.state.mail} onChange={this.changeName} name="mail"/>: this.state.mail}</dd>
-          </dl>
-          <dl>
-            <dt>学院</dt>
-            <dd>{this.state.flag ? <input value = {this.state.college} onChange={this.changeName} name="college" />: this.state.college}</dd>
-          </dl>
-          <dl>
-            <dt>专业</dt>
-            <dd>{this.state.flag ? <input value = {this.state.major} onChange={this.changeName} name="major" />: this.state.major}</dd>
-          </dl>
-          <dl>
-            <dt>年级</dt>
-            <dd>{this.state.flag ? <input value = {this.state.grade} onChange={this.changeName} name="grade"/>: this.state.grade}</dd>
-          </dl>
+        <div className="BasicInformationList">
+          {
+            this.state.BasicInformationList.map(item =>
+              <dl>
+                <dt>{item.key}</dt>
+                <dd>{this.state.flag? <input value={item.value} onChange={this.changeValue} name={item.key}/>:item.value}</dd>
+              </dl>
+              )
+          }
         </div>
       </div>
     )
