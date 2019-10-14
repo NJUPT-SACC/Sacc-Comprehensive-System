701c53b1750b547b8e72abb0727db449df3b8377
wwqin <245432372@qq.com>
2019-10-14 17:49:11 +0800

    fix:for-editor's wrap

--- pages/competition/competitionArena/components/competitionArenaStartItem.js
+++ pages/competition/competitionArena/components/competitionArenaStartItem.js
@@ -11,48 +11,7 @@ class CompetitionArenaStartItem extends React.Component{
     super(props);
     this.state = {
       answers:null,
-      value: `
-      import App, { Container } from 'next/app';
-      import React from 'react';
-      import { Provider } from 'react-redux';
-      import withReduxStore from '../store/with-redux-store'
-      import Router, {withRouter} from 'next/router'
-      import withRedux from 'next-redux-wrapper'
-      import Head from 'next/head';
-      import createStore from '../store/index';
-      import './index.less'
-      import '../static/fonts/iconfont.css'
-      import { persistStore } from 'redux-persist'
-      import { PersistGate } from 'redux-persist/integration/react'
-      
-      class MyApp extends App {
-        constructor (props) {
-          super(props)
-          this.persistor = persistStore(props.reduxStore)
-        }
-        render() {
-          const { Component, pageProps, router, reduxStore } = this.props;
-          return (
-            <Container>
-              <Head>
-                <title>SACC of Integrated system</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <link href="https://fonts.googleapis.com/css?family=Baloo+Paaji&display=swap" rel="stylesheet" />
-              </Head>
-              <Provider store={reduxStore}>
-                <PersistGate
-                  loading={<Component {...pageProps} />}
-                  persistor={this.persistor}
-                >
-                  <Component {...pageProps} />
-                </PersistGate>
-              </Provider>
-            </Container>
-          );
-        }
-      }
-      export default withReduxStore(MyApp)
-      `
+      value: ``
     }
   }
   changeOption = (e) =>{
@@ -62,6 +21,11 @@ class CompetitionArenaStartItem extends React.Component{
     e.target.style.background = '#fff';
     this.setState({
       answers:e.target.index
+    })
+  }
+  handleChange = (value) =>{
+    this.setState({
+      value
     })
   }
   componentDidMount(){
@@ -86,8 +50,8 @@ class CompetitionArenaStartItem extends React.Component{
       subfield: false, // 单双栏模式
     }
     const ediotstyle = {
+      wordWrap:'break-word'
     }
-    console.log(Editor)
 		return (
 			<div className="CompetitionArenaStartItem-main">
         <div className="CompetitionArenaStartItem-title"><Icon type="question-circle" />1、神秘的树莓派<span>[单选题]</span></div>
